0a759453dfc5bd1eada6b692f002e2d339446281
Lucas <lucas.simoura@goias.gov.br>
2025-01-21 16:34:06 -0300

    ChessException e ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
 import chess.ChessMatch;
-import chess.ChessPiece;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces()); 
+		UI.printBoard(chessMatch.getPieces());  
 	} 
 
 }
